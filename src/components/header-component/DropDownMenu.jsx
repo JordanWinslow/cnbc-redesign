67ec582d94c1605f9ccc1028e15d57da1fdb28a6
Jordan Winslow <admin@jordanwinslow.me>
2019-11-03 14:47:32 -0800

    Added Drop Down Nav Component!

---
+++ src/components/header-component/DropDownMenu.jsx
@@ -0,0 +1,253 @@
+import React from "react"
+import PropTypes from "prop-types"
+import styled from "styled-components"
+
+const Container = styled.div`
+  position: absolute;
+  z-index: 999;
+  top: 2.5rem;
+  background-color: var(--white);
+  box-shadow: 4px 4px 5px rgba(0, 0, 0, 0.3);
+  min-width: 15rem;
+`
+const Link = styled.div`
+  color: var(--black);
+  padding: 15px 20px;
+  font-family: "ProximaNova-Bold";
+  font-size: 17px;
+  border-bottom: 1px solid #ebebeb;
+  transition: 0.3s ease-in-out;
+  :hover {
+    background-color: var(--black);
+    color: var(--secondary);
+  }
+`
+
+const DropDownMenu = ({ category }) => {
+  return (
+    <Container>
+      {links[category].map(link => {
+        return <Link href={link.link}>{link.title}</Link>
+      })}
+    </Container>
+  )
+}
+DropDownMenu.propTypes = {
+  links: PropTypes.object.isRequired,
+  category: PropTypes.string.isRequired
+}
+const links = {
+  markets: [
+    {
+      title: "PRE-MARKETS",
+      link: "#"
+    },
+    {
+      title: "U.S.MARKETS",
+      link: "#"
+    },
+    {
+      title: "CURRENCIES",
+      link: "#"
+    },
+    {
+      title: "CRYPTOCURRENCY",
+      link: "#"
+    },
+    {
+      title: "FUTURES & COMMODITIES",
+      link: "#"
+    },
+    {
+      title: "BONDS",
+      link: "#"
+    },
+    {
+      title: "FUNDS & ETFS",
+      link: "#"
+    }
+  ],
+  business: [
+    {
+      title: "ECONOMY",
+      link: "#"
+    },
+    {
+      title: "FINANCE",
+      link: "#"
+    },
+    {
+      title: "HEALTH & SCIENCE",
+      link: "#"
+    },
+    {
+      title: "MEDIA",
+      link: "#"
+    },
+    {
+      title: "REAL ESTATE",
+      link: "#"
+    },
+    {
+      title: "ENERGY",
+      link: "#"
+    },
+    {
+      title: "TRANSPORTATION",
+      link: "#"
+    },
+    {
+      title: "INDUSTRIALS",
+      link: "#"
+    },
+    {
+      title: "RETAIL",
+      link: "#"
+    },
+    {
+      title: "WEALTH",
+      link: "#"
+    },
+    {
+      title: "SMALL BUSINESS",
+      link: "#"
+    }
+  ],
+  investing: [
+    {
+      title: "INVEST IN YOU",
+      link: "#"
+    },
+    {
+      title: "PERSONAL FINANCE",
+      link: "#"
+    },
+    {
+      title: "FINANCIAL ADVISORS",
+      link: "#"
+    },
+    {
+      title: "TRADING NATION",
+      link: "#"
+    },
+    {
+      title: "OPTIONS ACTION",
+      link: "#"
+    },
+    {
+      title: "ETF STREET",
+      link: "#"
+    },
+    {
+      title: "BUFFETT ARCHIVE",
+      link: "#"
+    },
+    {
+      title: "EARNINGS",
+      link: "#"
+    },
+    {
+      title: "TRADER TALK",
+      link: "#"
+    }
+  ],
+  tech: [
+    {
+      title: "CYBERSECURITY",
+      link: "#"
+    },
+    {
+      title: "ENTERPRISE",
+      link: "#"
+    },
+    {
+      title: "INTERNET",
+      link: "#"
+    },
+    {
+      title: "MEDIA",
+      link: "#"
+    },
+    {
+      title: "MOBILE",
+      link: "#"
+    },
+    {
+      title: "SOCIAL MEDIA",
+      link: "#"
+    },
+    {
+      title: "VENTURE CAPITAL",
+      link: "#"
+    },
+    {
+      title: "TECH GUIDE",
+      link: "#"
+    }
+  ],
+  politics: [
+    {
+      title: "WHITE HOUSE",
+      link: "#"
+    },
+    {
+      title: "POLICY",
+      link: "#"
+    },
+    {
+      title: "DEFENSE",
+      link: "#"
+    },
+    {
+      title: "CONGRESS",
+      link: "#"
+    },
+    {
+      title: "2020 ELECTIONS",
+      link: "#"
+    }
+  ],
+  cnbcTV: [
+    {
+      title: "LIVE TV",
+      link: "#"
+    },
+    {
+      title: "LIVE AUDIO",
+      link: "#"
+    },
+    {
+      title: "LATEST VIDEO",
+      link: "#"
+    },
+    {
+      title: "TOP VIDEO",
+      link: "#"
+    },
+    {
+      title: "CEO INTERVIEWS",
+      link: "#"
+    },
+    {
+      title: "BUSINESS DAY SHOWS",
+      link: "#"
+    },
+    {
+      title: "PRIMETIME SHOWS",
+      link: "#"
+    },
+    {
+      title: "CNBC WORLD",
+      link: "#"
+    },
+    {
+      title: "DIGITAL ORIGINALS",
+      link: "#"
+    },
+    {
+      title: "FULL EPISODES",
+      link: "#"
+    }
+  ]
+}
+export default DropDownMenu
--- src/components/header-component/NavLinks.jsx
+++ src/components/header-component/NavLinks.jsx
@@ -1,5 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
+
+import DropDownMenu from "./DropDownMenu"
 
 const Container = styled.div`
   width: 53vw;
@@ -42,16 +44,58 @@ const Link = styled.a`
   }
 `
 const NavLinks = () => {
+  const [visible, setVisible] = useState(false)
   return (
     <Container>
-      <Link href="#">MARKETS</Link>
-      <Link href="#">BUSINESS</Link>
-      <Link href="#">INVESTING</Link>
-      <Link href="#">TECH</Link>
-      <Link href="#">POLITICS</Link>
-      <Link href="#">CNBC TV</Link>
+      <Link
+        href="#"
+        onMouseOver={() => setVisible({ markets: true })}
+        onMouseLeave={() => setVisible({ markets: false })}
+      >
+        MARKETS
+        {visible.markets && <DropDownMenu category="markets" />}
+      </Link>
+      <Link
+        href="#"
+        onMouseOver={() => setVisible({ business: true })}
+        onMouseLeave={() => setVisible({ business: false })}
+      >
+        BUSINESS
+        {visible.business && <DropDownMenu category="business" />}
+      </Link>
+      <Link
+        href="#"
+        onMouseOver={() => setVisible({ investing: true })}
+        onMouseLeave={() => setVisible({ investing: false })}
+      >
+        INVESTING
+        {visible.investing && <DropDownMenu category="investing" />}
+      </Link>
+      <Link
+        href="#"
+        onMouseOver={() => setVisible({ tech: true })}
+        onMouseLeave={() => setVisible({ tech: false })}
+      >
+        TECH
+        {visible.tech && <DropDownMenu category="tech" />}
+      </Link>
+      <Link
+        href="#"
+        onMouseOver={() => setVisible({ politics: true })}
+        onMouseLeave={() => setVisible({ politics: false })}
+      >
+        POLITICS
+        {visible.politics && <DropDownMenu category="politics" />}
+      </Link>
+      <Link
+        href="#"
+        onMouseOver={() => setVisible({ cnbcTV: true })}
+        onMouseLeave={() => setVisible({ cnbcTV: false })}
+      >
+        CNBC TV
+        {visible.cnbcTV && <DropDownMenu category="cnbcTV" />}
+      </Link>
     </Container>
   )
 }
-
 export default NavLinks
